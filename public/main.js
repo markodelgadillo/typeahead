8f49dd27d03c25ed7e38e8f2dad8760fa0d69abd
marko delgadillo <markoneseven@gmail.com>
2017-10-30 15:13:43 -0700

    #1 The list of possible matches will render as more or less text is inputted into the input field.

--- public/main.js
+++ public/main.js
@@ -3,59 +3,42 @@ const list = () => fetch('/country').then(res => res.json())
 list().then(documents => (country = documents))
 let country = ''
 
-function renderName(name) {
+function renderNames(names) {
   const $result = document.querySelector('#result')
-  $result.textContent = name
+  $result.innerHTML = ''
+  names.forEach(name => {
+    const $div = document.createElement('div')
+    $div.textContent = name
+    $result.appendChild($div)
+  })
 }
-
-// function compare(arr) {
-//   arr = country
-//   for (let i = 0; i < arr.length; i++) {
-//     if ($term.value === arr[i].name.toLowerCase()) {
-//       renderName(arr[i].name)
-//     }
-//   }
-// }
 
 $term.addEventListener('keyup', function() {
   console.log($term.value)
-  compareThree()
+  compare()
 })
 
-// test
-// function compareTwo(letters) {
-//   arr = country
-//   for (let i = 0; i < arr.length; i++) {
-//     let name = arr[i].name
-//     for (let j = 0; j < name.length; j++) {
-//       if ((letters = characters)) {
-//         renderName(name)
-//       }
-//     }
-//   }
-// }
-
-function compareThree(arr) {
-  arr = country
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].name.toLowerCase().startsWith($term.value)) {
-      renderName(arr[i].name)
+function compare() {
+  if ($term.value) {
+    let list = []
+    for (let i = 0; i < country.length; i++) {
+      if (country[i].name.toLowerCase().startsWith($term.value)) {
+        list.push(country[i].name)
+        console.log(list)
+      }
     }
+    renderNames(list)
+  } else {
+    const $result = document.querySelector('#result')
+    $result.innerHTML = ''
   }
 }
 
-/* notes
-- database with array
-- string.prototype.indexOf()
-- id #term for search term
-- toLowerCase
-- charAt?
-- array.filter(word => {
-    if (word.indexOf('T')===0})
+/*
+adjust input field with spacing around it separating the results and
+input field
 
-- search function to mongodb
-- use delay search upon key press (debouncing)
-- begins search after two letters inputted
-- on key down event listener
--
+async fetch?
+
+
 */
