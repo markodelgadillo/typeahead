1c4ac2f722e3ad95bba388987237718ac071dd65
marko delgadillo <markoneseven@gmail.com>
2017-11-15 15:21:21 -0800

    #2 Initial commit of all necessary files.

---
+++ client/main.js
@@ -0,0 +1,36 @@
+const $term = document.querySelector('#term')
+const list = () => fetch('/country').then(res => res.json())
+list().then(documents => (country = documents))
+let country = ''
+
+function renderNames(names) {
+  const $result = document.querySelector('.result')
+  $result.innerHTML = ''
+  names.forEach(name => {
+    const $div = document.createElement('div')
+    $div.setAttribute('class', 'list')
+    $div.textContent = name
+    $result.appendChild($div)
+  })
+}
+
+$term.addEventListener('keyup', function() {
+  console.log($term.value)
+  compare()
+})
+
+function compare() {
+  if ($term.value) {
+    let list = []
+    for (let i = 0; i < country.length; i++) {
+      if (country[i].name.toLowerCase().startsWith($term.value)) {
+        list.push(country[i].name)
+        console.log(list)
+      }
+    }
+    renderNames(list)
+  } else {
+    const $result = document.querySelector('.result')
+    $result.innerHTML = ''
+  }
+}
