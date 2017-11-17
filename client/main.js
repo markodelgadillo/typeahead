facfc1d9991eff9d573cfb3933570fc841f101d9
marko delgadillo <markoneseven@gmail.com>
2017-11-17 09:35:15 -0800

    #2 Refactored with react jsx.

--- client/main.js
+++ client/main.js
@@ -1,36 +1,55 @@
-const $term = document.querySelector('#term')
-const list = () => fetch('/country').then(res => res.json())
-list().then(documents => (country = documents))
-let country = ''
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-function renderNames(names) {
-  const $result = document.querySelector('.result')
-  $result.innerHTML = ''
-  names.forEach(name => {
-    const $div = document.createElement('div')
-    $div.setAttribute('class', 'list')
-    $div.textContent = name
-    $result.appendChild($div)
-  })
-}
-
-$term.addEventListener('keyup', function() {
-  console.log($term.value)
-  compare()
-})
-
-function compare() {
-  if ($term.value) {
-    let list = []
-    for (let i = 0; i < country.length; i++) {
-      if (country[i].name.toLowerCase().startsWith($term.value)) {
-        list.push(country[i].name)
-        console.log(list)
-      }
+class Typeahead extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleCountryList = this.handleCountryList.bind(this)
+    this.state = {
+      value: '',
+      countries: [],
+      matches: []
     }
-    renderNames(list)
-  } else {
-    const $result = document.querySelector('.result')
-    $result.innerHTML = ''
+  }
+
+  componentWillMount() {
+    const country = () => {
+      return fetch('http://localhost:7000/country').then(res => res.json())
+    }
+    country().then(countries => this.setState({ countries: countries }))
+  }
+
+  handleOnChange(event) {
+    console.log(event.target.value)
+    this.setState({ value: event.target.value })
+    this.setState({
+      matches: this.state.countries.filter(
+        ({ name }) =>
+          !(this.state.value === '') && name.startsWith(this.state.value)
+      )
+    })
+  }
+
+  handleCountryList() {}
+
+  render() {
+    console.log(this.state.matches)
+    return (
+      <div>
+        <input onChange={this.handleOnChange} />
+        {this.state.matches.length > 0 && (
+          <div>
+            <ul>
+              {this.state.matches.map(({ name: match }) => (
+                <li key={match}>{match}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    )
   }
 }
+
+render(<Typeahead />, document.querySelector('.container'))
